438cb2df9525593b3b6e3f550146715ce3d45614
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2017-02-03 13:31:44 -0800

    Create Pokemon class, set up getters

---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,30 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by spencerdezartsmith on 2/3/17.
+//  Copyright © 2017 spencerdezartsmith. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    // Set up getters
+    var name: String {
+        
+        return _name
+    }
+    
+    var pokedexId: Int {
+        
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
